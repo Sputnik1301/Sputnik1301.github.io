c16f12e5b7418efd590a71671038b006dc6b23a9
Sputnik1301 <109699239+Sputnik1301@users.noreply.github.com>
2024-04-18 20:52:33 +0100

    Add files via upload
    NEW FILES 18/4 V1

---
+++ DevilsOwn/js/homepage.js
@@ -0,0 +1,76 @@
+
+
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+const overlay = document.querySelector("#overlay");
+const nav1 = document.querySelector("#nav-1");
+const nav2 = document.querySelector("#nav-2");
+const nav3 = document.querySelector("#nav-3");
+const nav4 = document.querySelector("#nav-4");
+const nav5 = document.querySelector("#nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// Control Navigation Animation
+function navAnimation(val1, val2) 
+{
+    navItems.forEach((nav, i) => {
+    nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
+    });
+}
+
+function toggleNav() 
+{
+  // Toggle: Hamburger Open/Close
+  hamburgerMenu.classList.toggle("active");
+
+  //   Toggle: Menu Active
+  overlay.classList.toggle("overlay-active");
+
+  if (overlay.classList.contains("overlay-active")) {
+    // Animate In - Overlay
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+
+    // Animate In - Nav Items
+    navAnimation("out", "in");
+  } else {
+    // Animate Out - Overlay
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+
+    // Animate Out - Nav Items
+    navAnimation("in", "out");
+  }
+}
+
+// Events Listeners
+hamburgerMenu.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});
+
+window.addEventListener('resize', arrangeEmbeds);
+
+function arrangeEmbeds() {
+  var container = document.getElementById('instagram-embeds');
+  var embeds = Array.from(container.children);
+  var currentRow = [];
+  var currentRowHeight = 0;
+  
+  embeds.forEach(function(embed, index) {
+    currentRow.push(embed);
+    
+    if (index === embeds.length - 1 || embeds[index + 1].offsetTop > embed.offsetTop) {
+      var topPosition = currentRow[0].offsetTop + currentRowHeight;
+      
+      currentRow.slice(1).forEach(function(embed) {
+        embed.style.position = 'absolute';
+        embed.style.top = topPosition + 'px';
+      });
+      
+      currentRow = [];
+      currentRowHeight = embed.offsetHeight;
+    } else {
+      currentRowHeight = Math.max(currentRowHeight, embed.offsetHeight);
+    }
+  });
+}
+
+arrangeEmbeds();
